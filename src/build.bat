747a0ed81c935806194f0efb90f209be20f43cb2
leo <lqz.leo@gmail.com>
2012-01-31 10:56:37 +0800

    change the build.bat for build Qi.NHibernate.Extender

--- src/build.bat
+++ src/build.bat
@@ -4,3 +4,6 @@ msbuild "%cd%\Qi\Qi\Qi.csproj"   /property:Configuration=Release /p:Configuratio
 
 msbuild "%cd%\Qi\Qi.Web\Qi.Web.csproj"   /property:Configuration=Debug /p:Configuration=Debug /p:OutputPath="%cd%\Publish\Debug"
 msbuild "%cd%\Qi\Qi.Web\Qi.Web.csproj"   /property:Configuration=Release /p:Configuration=Release /p:OutputPath="%cd%\Publish\Release"
+
+msbuild "%cd%\Qi\Qi.NHibernateExtender\Qi.NHibernateExtender.csproj"   /property:Configuration=Debug /p:Configuration=Debug /p:OutputPath="%cd%\Publish\Debug"
+msbuild "%cd%\Qi\Qi.NHibernateExtender\Qi.NHibernateExtender.csproj"   /property:Configuration=Release /p:Configuration=Release /p:OutputPath="%cd%\Publish\Release"

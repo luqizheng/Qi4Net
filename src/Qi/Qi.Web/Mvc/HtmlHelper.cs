9c623e6d2c8a1c1cb56829e57120eb8c2cee8e54
leo <lqz.leo@gmail.com>
2012-02-02 14:48:49 +0800

    add HtmlHelper

---
+++ src/Qi/Qi.Web/Mvc/HtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Qi.Web.Mvc
+{
+    public static class HtmlHelper
+    {
+        public static MvcHtmlString LabelForModel<T, TValue>(this HtmlHelper<T> helper, T model,
+                                                             Expression<Func<T, TValue>> expression)
+        {
+            return helper.LabelFor(expression);
+        }
+    }
+}

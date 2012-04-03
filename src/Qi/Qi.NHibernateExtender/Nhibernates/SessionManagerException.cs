705865992fe2e083612f631c61491866c2bab6f3
leo <lqz.leo@gmail.com>
2012-04-03 09:24:28 +0800

    add sessionManagerException for SessionManager exception.

---
+++ src/Qi/Qi.NHibernateExtender/Nhibernates/SessionManagerException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qi.Nhibernates
+{
+    [Serializable]
+    public class SessionManagerException:ApplicationException
+    {
+        public SessionManagerException(string message):base(message)
+        {
+            
+        }
+    }
+}

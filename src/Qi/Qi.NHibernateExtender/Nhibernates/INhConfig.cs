eb9a1e9b506af6b2f2455c0aa21ae5fbadb65561
luqizheng <luqizheng@126.com>
2012-04-13 11:59:14 +0800

    add lost file.

---
+++ src/Qi/Qi.NHibernateExtender/Nhibernates/INhConfig.cs
@@ -0,0 +1,33 @@
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace Qi.Nhibernates
+{
+    public interface INhConfig
+    {
+        /// <summary>
+        /// Gets the SessionFactoryName 
+        /// </summary>
+        string SessionFactoryName { get; }
+
+        /// <summary>
+        /// Gets the Nhibernate Configuration from this setting file.
+        /// </summary>
+        Configuration NHConfiguration { get; }
+
+        /// <summary>
+        /// Get the value indecate file is changed or not.
+        /// </summary>
+        bool IsChanged { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ISessionFactory BuildSessionFactory();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Refresh();
+    }
+}

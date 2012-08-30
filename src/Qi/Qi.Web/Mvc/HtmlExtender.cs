2b0573cdc24f40b0763984e600d94733a418539e
luqizheng@126.com <luqizheng@126.com>
2012-08-30 22:47:20 +0800

    up to mvc 4.0 support.

--- src/Qi/Qi.Web/Mvc/HtmlExtender.cs
+++ src/Qi/Qi.Web/Mvc/HtmlExtender.cs
@@ -9,43 +9,43 @@ namespace Qi.Web.Mvc
 {
     public static class HtmlExtender
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        ///<remarks>
-        /// code from http://weblogs.asp.net/imranbaloch/archive/2010/07/03/asp-net-mvc-labelfor-helper-with-htmlattributes.aspx
-        /// </remarks>
-        public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html,
-                                                             Expression<Func<TModel, TValue>> expression,
-                                                             object htmlAttributes)
-        {
-            return LabelFor(html, expression, new RouteValueDictionary(htmlAttributes));
-        }
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        /////<remarks>
+        ///// code from http://weblogs.asp.net/imranbaloch/archive/2010/07/03/asp-net-mvc-labelfor-helper-with-htmlattributes.aspx
+        ///// </remarks>
+        //public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html,
+        //                                                     Expression<Func<TModel, TValue>> expression,
+        //                                                     object htmlAttributes)
+        //{
+        //    return LabelFor(html, expression, new RouteValueDictionary(htmlAttributes));
+        //}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        ///<remarks>
-        /// code from http://weblogs.asp.net/imranbaloch/archive/2010/07/03/asp-net-mvc-labelfor-helper-with-htmlattributes.aspx
-        /// </remarks>
-        public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html,
-                                                             Expression<Func<TModel, TValue>> expression,
-                                                             IDictionary<string, object> htmlAttributes)
-        {
-            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
-            string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
-            string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
-            if (String.IsNullOrEmpty(labelText))
-            {
-                return MvcHtmlString.Empty;
-            }
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        /////<remarks>
+        ///// code from http://weblogs.asp.net/imranbaloch/archive/2010/07/03/asp-net-mvc-labelfor-helper-with-htmlattributes.aspx
+        ///// </remarks>
+        //public static MvcHtmlString LabelFor<TModel, TValue>(this HtmlHelper<TModel> html,
+        //                                                     Expression<Func<TModel, TValue>> expression,
+        //                                                     IDictionary<string, object> htmlAttributes)
+        //{
+        //    ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+        //    string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+        //    string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+        //    if (String.IsNullOrEmpty(labelText))
+        //    {
+        //        return MvcHtmlString.Empty;
+        //    }
 
-            var tag = new TagBuilder("label");
-            tag.MergeAttributes(htmlAttributes);
-            tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
-            tag.SetInnerText(labelText);
-            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
-        }
+        //    var tag = new TagBuilder("label");
+        //    tag.MergeAttributes(htmlAttributes);
+        //    tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+        //    tag.SetInnerText(labelText);
+        //    return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        //}
         /// <summary>
         /// Out put the expression.
         /// </summary>

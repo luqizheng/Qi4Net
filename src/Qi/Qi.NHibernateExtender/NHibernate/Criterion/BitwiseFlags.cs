e771d51d44a316b37c533d765b8d315fdb785bbf
luqizheng <luqizheng@126.com>
2012-09-18 11:48:44 +0800

    add BitwiseFlags for bitwise operator. code from http://stackoverflow.com/questions/1835392/hql-to-criteriaquery-when-using-bitwise-operators

---
+++ src/Qi/Qi.NHibernateExtender/NHibernate/Criterion/BitwiseFlags.cs
@@ -0,0 +1,28 @@
+using System;
+using NHibernate;
+using NHibernate.Criterion;
+
+namespace Qi.NHibernate.Criterion
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BitwiseFlags : LogicalExpression
+    {
+        private BitwiseFlags(string propertyName, object value, string op) :
+            base(new SimpleExpression(propertyName, value, op),
+                 Expression.Sql("?", value, NHibernateUtil.Enum(value.GetType())))
+        {
+        }
+
+        protected override string Op
+        {
+            get { return "="; }
+        }
+
+        public static BitwiseFlags IsSet(string propertyName, Enum flags)
+        {
+            return new BitwiseFlags(propertyName, flags, " & ");
+        }
+    }
+}

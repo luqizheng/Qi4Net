bc1d0c19e7027963a6513208f909ad1a791ebd79
luqizheng@126.com <luqizheng@126.com>
2012-10-08 22:30:49 +0800

    bug fix ResizePic.cs. it should release all resource.

--- src/Qi/Qi/Draw/ResizePic.cs
+++ src/Qi/Qi/Draw/ResizePic.cs
@@ -30,50 +30,63 @@ namespace Qi.Draw
         {
             Image originalImage = Image.FromFile(originalImagePath);
 
-
-            int targetWidth;
-            int targetHight;
-
-            if (originalImage.Height >= originalImage.Width)
+            try
             {
-                if (originalImage.Height < MaxHeight)
+                int targetWidth;
+                int targetHight;
+
+                if (originalImage.Height >= originalImage.Width)
                 {
-                    File.Copy(originalImagePath, thumNailPath);
+                    if (originalImage.Height < MaxHeight)
+                    {
+                        File.Copy(originalImagePath, thumNailPath);
+                    }
+                    targetHight = MaxHeight;
+                    targetWidth = originalImage.Width * targetHight / originalImage.Height;
                 }
-                targetHight = MaxHeight;
-                targetWidth = originalImage.Width*targetHight/originalImage.Height;
+                else
+                {
+                    if (originalImage.Width < MaxWidth)
+                    {
+                        File.Copy(originalImagePath, thumNailPath);
+                    }
+                    targetWidth = MaxWidth;
+                    targetHight = originalImage.Height * targetWidth / originalImage.Width;
+                }
+
+
+                //新建一个bmp图片
+                using (var bitmap = new Bitmap(targetWidth, targetWidth))
+                {
+
+                    //新建一个画板
+                    using (Graphics graphic = Graphics.FromImage(bitmap))
+                    {
+
+                        //设置高质量查值法
+                        graphic.InterpolationMode = InterpolationMode.High;
+
+                        //设置高质量，低速度呈现平滑程度
+                        graphic.SmoothingMode = SmoothingMode.HighQuality;
+
+                        //清空画布并以透明背景色填充
+                        graphic.Clear(Color.Transparent);
+
+                        //在指定位置并且按指定大小绘制原图片的指定部分
+                        graphic.DrawImage(originalImage, new Rectangle(0, 0, targetWidth, targetHight),
+                                          new Rectangle(0, 0, originalImage.Width, originalImage.Height),
+                                          GraphicsUnit.Pixel);
+                        bitmap.Save(thumNailPath, ImageFormat.Png);
+                    }
+
+                }
             }
-            else
+            finally
             {
-                if (originalImage.Width < MaxWidth)
-                {
-                    File.Copy(originalImagePath, thumNailPath);
-                }
-                targetWidth = MaxWidth;
-                targetHight = originalImage.Height*targetWidth/originalImage.Width;
+                originalImage.Dispose();
             }
 
 
-            //新建一个bmp图片
-            var bitmap = new Bitmap(targetWidth, targetWidth);
-
-            //新建一个画板
-            Graphics graphic = Graphics.FromImage(bitmap);
-
-            //设置高质量查值法
-            graphic.InterpolationMode = InterpolationMode.High;
-
-            //设置高质量，低速度呈现平滑程度
-            graphic.SmoothingMode = SmoothingMode.HighQuality;
-
-            //清空画布并以透明背景色填充
-            graphic.Clear(Color.Transparent);
-
-            //在指定位置并且按指定大小绘制原图片的指定部分
-            graphic.DrawImage(originalImage, new Rectangle(0, 0, targetWidth, targetHight),
-                              new Rectangle(0, 0, originalImage.Width, originalImage.Height),
-                              GraphicsUnit.Pixel);
-            bitmap.Save(thumNailPath, ImageFormat.Png);
         }
     }
 }

ee3ce7d417d2fe5316f7a521aa4abb9272d66415
leo.lu <luqizheng@126.com>
2012-11-18 22:54:23 +0800

    build the lib

---
+++ src/Qi/Mvc4Test/Controllers/NHModelBinerTestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc4Test.Models;
+
+namespace Mvc4Test.Controllers
+{
+    public class NHModelBinerTestController : Controller
+    {
+        //
+        // GET: /NHModelBinerTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ArrayBinder()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ArrayBinder(Role[] roles)
+        {
+            return View();
+        }
+
+        public ActionResult DtoPropertyBinder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult ArrayProperty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

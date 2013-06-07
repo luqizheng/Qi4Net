fe9c3372002d2ecf5105aaabe7daa7e5966f0583
leo <lqz.leo@gmail.com>
2013-06-07 15:51:42 +0800

    update nhibernate to 3.3.3.4001

--- src/NuGet_Build.bat
+++ src/NuGet_Build.bat
@@ -1,7 +1,7 @@
 
 Qi_src\.nuget\nuget pack Qi_src/Qi/Qi.csproj  -Prop Configuration=Release -build
 
-@rem Qi_src\.nuget\nuget pack Qi_src/Qi.Web/Qi.Web.csproj  -Prop Configuration=Release -build
-@rem Qi_src\.nuget\nuget pack Qi_src/Qi.NHibernateExtender/Qi.NHibernateExtender.csproj  -Prop Configuration=Release -build
+Qi_src\.nuget\nuget pack Qi_src/Qi.Web/Qi.Web.csproj  -Prop Configuration=Release -build
+Qi_src\.nuget\nuget pack Qi_src/Qi.NHibernateExtender/Qi.NHibernateExtender.csproj  -Prop Configuration=Release -build
 
 

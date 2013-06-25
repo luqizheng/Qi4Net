1b63dfaed028f9e0457fad11aa8ad108f5d622ab
leo <lqz.leo@gmail.com>
2013-06-25 09:33:04 +0800

    add Cr3eateInstanceHelper.cs

---
+++ src/Qi_src/Qi.NHibernateExtender/Web/Mvc/CreateInstanceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Qi.Web.Mvc
+{
+    internal static class CreateInstanceHelper
+    {
+        public static object CreateInstance(Type modelType)
+        {
+            ConstructorInfo constructor = modelType
+                    .GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+                                    null, new Type[0], new ParameterModifier[0]);
+            return constructor.Invoke(null);
+        }
+    }
+}

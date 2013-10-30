5ee6a600353853753e03531d6bcef7b261e1c63c
lqz <luqizheng@126.com>
2013-10-30 14:12:15 +0800

    add Sort and search class.

--- src/Qi_src/Qi.NHibernateExtender/SortDirect.cs
+++ src/Qi_src/Qi.NHibernateExtender/SortDirect.cs
@@ -3,16 +3,75 @@ using System.Linq.Expressions;
 
 namespace Qi
 {
+    /// <summary>
+    /// </summary>
     public enum SortDirect
     {
+        /// <summary>
+        /// </summary>
         Asc,
+
+        /// <summary>
+        /// </summary>
         Desc
     }
 
-
+    /// <summary>
+    /// </summary>
     public class SortTarget
     {
+        /// <summary>
+        /// </summary>
         public SortDirect Tag { get; set; }
+
+        /// <summary>
+        /// </summary>
         public Expression<Func<object>> Property { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public Symbol Symbol { get; set; }
+    }
+
+    /// <summary>
+    /// </summary>
+    public class SearchCondition
+    {
+        /// <summary>
+        /// </summary>
+        public object Value { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public Expression<Func<object>> Property { get; set; }
+    }
+
+    /// <summary>
+    /// </summary>
+    public enum Symbol
+    {
+        /// <summary>
+        /// </summary>
+        Equal,
+
+        /// <summary>
+        /// </summary>
+        Like,
+
+        /// <summary>
+        /// </summary>
+        GreaterThan,
+
+        /// <summary>
+        /// </summary>
+        GreaterEqual,
+
+        /// <summary>
+        /// </summary>
+        LessThan,
+
+        /// <summary>
+        /// </summary>
+        LessEqual,
     }
 }

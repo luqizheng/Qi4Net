e6e540c332ebdde472b4d396fd01e10aacdf83cb
lqz <luqizheng@126.com>
2013-11-19 17:14:34 +0800

    增加Ip端检查

--- src/Qi_src/Qi/Net/IpAddressExtender.cs
+++ src/Qi_src/Qi/Net/IpAddressExtender.cs
@@ -6,51 +6,35 @@ namespace Qi.Net
 {
     public static class IpAddressExtender
     {
-        [DllImport("Iphlpapi.dll")]
-        private static extern int SendARP(Int32 dest, Int32 host, ref Int64 mac, ref Int32 length);
-
-        [DllImport("Ws2_32.dll")]
-        private static extern Int32 inet_addr(string ip);
-
         /// <summary>
-        /// 通过IPAddress 获取Mac地址，只是用于同网段
+        /// Check ip is in min to max;
         /// </summary>
-        /// <param name="client"></param>
+        /// <param name="ip"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
         /// <returns></returns>
-        /// <exception cref="NetworkException">failed to get client mac</exception>
-        public static string GetMac(this IPAddress client)
+        public static bool In(this IPAddress ip, IPAddress min, IPAddress max)
         {
-            if (client == null)
-                throw new ArgumentNullException("client");
-            try
-            {
-                int ldest = inet_addr(client.ToString()); //目的地的ip
+            var lip = ip.ToInt64();
+            var lmin = min.ToInt64();
+            var lMax = min.ToInt64();
+            return lip <= lMax && lip >= lmin;
+        }
 
-                var macinfo = new Int64();
-                int len = 6;
-                int res = SendARP(ldest, 0, ref macinfo, ref len);
-                string macSrc = macinfo.ToString("X");
-                string macDest = "";
+        public static long ToInt64(this IPAddress ip)
+        {
+            int x = 3;
 
-                while (macSrc.Length < 12)
-                {
-                    macSrc = macSrc.Insert(0, "0");
-                }
-                for (int i = 0; i < 11; i++)
-                {
-                    if (0 == (i%2))
-                    {
-                        macDest = i == 10
-                                      ? macDest.Insert(0, macSrc.Substring(i, 2))
-                                      : "-" + macDest.Insert(0, macSrc.Substring(i, 2));
-                    }
-                }
-                return macDest;
-            }
-            catch (Exception ex)
+            long o = 0;
+
+            foreach (byte f in ip.GetAddressBytes())
             {
-                throw new NetworkException(String.Format("Can't find mac from {0}", client), ex);
+
+                o += (long)f << 8 * x--;
+
             }
+
+            return o;
         }
     }
 }

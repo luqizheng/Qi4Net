4169e58f5aa1d495c9311008fb50f179a44fe52d
lqz <luqizheng@126.com>
2014-04-12 09:52:03 +0800

    add lost file.

---
+++ src/Qi_src/Qi.Web/Mvc/JQueryArrayFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Qi.Web.Mvc
+{
+    public class JQueryArrayFilter : ActionFilterAttribute
+    {
+        private readonly string[] _postNames;
+
+        public JQueryArrayFilter(params string[] postNames)
+        {
+            if (postNames == null)
+                throw new ArgumentNullException("postNames");
+            _postNames = postNames;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            foreach (string key in _postNames)
+            {
+                string aryKey = key + "[]";
+                ValueProviderResult valueProviderResult = filterContext.Controller.ValueProvider.GetValue(aryKey);
+                if (valueProviderResult == null)
+                    continue;
+                filterContext.ActionParameters[key] = valueProviderResult.RawValue;
+            }
+        }
+    }
+}
---
+++ src/Qi_src/Qi/TypeUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qi
+{
+    public static class TypeUtility
+    {
+        public static bool IsInteger(ValueType value)
+        {
+            return (value is SByte || value is Int16 || value is Int32
+                    || value is Int64 || value is Byte || value is UInt16
+                    || value is UInt32 || value is UInt64);
+        }
+
+        public static bool IsFloat(ValueType value)
+        {
+            return (value is float | value is double | value is Decimal);
+        }
+
+        public static bool IsNumeric(Type value)
+        {
+            if (!(value == typeof(Byte) ||
+                    value == typeof(Int16) ||
+                    value == typeof(Int32) ||
+                    value == typeof(Int64) ||
+                    value == typeof(SByte) ||
+                    value == typeof(UInt16) ||
+                    value == typeof(UInt32) ||
+                    value == typeof(UInt64) ||
+                    value == typeof(Decimal) ||
+                    value == typeof(Double) ||
+                    value == typeof(Single)))
+                return false;
+            else
+                return true;
+        }
+
+        public static bool IsBasicType(Type t)
+        {
+            return t == typeof(string) || t.IsEnum ||  t == typeof(bool) ||IsNumeric(t) ;
+        }
+    }
+}
